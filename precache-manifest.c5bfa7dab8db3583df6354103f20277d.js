6c6bcb572639b22df5353f998394068d09522e0f
Maciej Ostrowski <Ostrowski@Mys-MacBook-Pro.local>
2020-05-26 10:02:28 +0200

    Updates

---
+++ precache-manifest.c5bfa7dab8db3583df6354103f20277d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e3f435e840ec8c7b23d11520d27cdee",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "81c60b3770546a60310e",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "bd04421ecbb3e94250c8",
+    "url": "/robofriends/static/css/main.84d365c7.chunk.css"
+  },
+  {
+    "revision": "81c60b3770546a60310e",
+    "url": "/robofriends/static/js/2.ce3203d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.ce3203d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd04421ecbb3e94250c8",
+    "url": "/robofriends/static/js/main.f53610d7.chunk.js"
+  },
+  {
+    "revision": "99557a09ab68caec6407",
+    "url": "/robofriends/static/js/runtime-main.229ccf3b.js"
+  }
+]);
